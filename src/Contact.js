744b3af15f55d4942ef3f5b20574afe3d0e1d841
unknown <pritomsaha24.ps@gmail.com>
2023-02-23 05:05:58 -0800

    contact img added

--- src/Contact.js
+++ src/Contact.js
@@ -1,57 +1,84 @@
-import React,{useEffect} from 'react'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import{faLocationDot } from '@fortawesome/free-solid-svg-icons'
-import{faPhone} from '@fortawesome/free-solid-svg-icons'
-import{faAt} from '@fortawesome/free-solid-svg-icons'
-import './infosection.css'
-import Aos from 'aos';
-
-import 'aos/dist/aos.css'
+import React, { useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faAt } from "@fortawesome/free-solid-svg-icons";
+import "./infosection.css";
+import Aos from "aos";
+import logo from "../src/image/contact-img.png";
+import "aos/dist/aos.css";
 function Contact() {
-	useEffect(()=>{
-		Aos.init({duration:3000})
-	  },[])
-  return<>
-	  <section className=" section contact-section">
-		  <div className='container'>
-			<div className="row">
-				<div className="col-12 text-center mb-2">
-					<h2 className="hero-title">Contact with me</h2>
-				</div>
-			</div>
-			<div className="row">
-				<div className="col-lg-5 col-md-5 order-md-last d-flex pl-3 pr-3">
-					<div className="contact-wrap w-100">
-						<form data-aos="fade-left" >
-							<h5>Send message</h5>
-							<div className='form-group'>
-								<span>Full Name</span>
-								<input type="text" className='form-control' name='' required="required"/>
-							</div>
-							<div className='form-group'>
-								<span>Email</span>
-								<input type="text" className='form-control' name='' required="required"/>
-							</div>
-							<div className='form-group'>
-								<span>Write your message</span>
-								<textarea required='required' className='form-control'></textarea>
-							</div>
-							<div className='form-group'>
-								<input type="submit" className='btn send-button' value='Send Message'/>
-							</div>
-						
-						</form>
-					</div>
-				</div>
-				<div className="col-lg-6 col-md-6 d-flex align-items-stretch contact-text">
-					<div data-aos="fade-right" className="info-wrap w-100 pl-5">
-						<h1 className='contact-author'> Sadia Afrin</h1>
-					</div>
-				</div>
-			</div>
-		  </div>
-	  </section>
-  </>
+  useEffect(() => {
+    Aos.init({ duration: 3000 });
+  }, []);
+  return (
+    <>
+      <section className=" section contact-section">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 text-center mb-2">
+              <h2 className="hero-title">Contact with me</h2>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-5 col-md-5 order-md-last d-flex pl-3 pr-3">
+              <div className="contact-wrap w-100">
+                <form data-aos="fade-left">
+                  <div className="form-group">
+                    <span>Full Name</span>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name=""
+                      required="required"
+                      placeholder="Enter Your Name"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <span>Email</span>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name=""
+                      required="required"
+                      placeholder="Enter Your Email"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <span>Write your message</span>
+                    <textarea
+                      required="required"
+                      className="form-control"
+                      placeholder="Write Your Message"
+                    ></textarea>
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="submit"
+                      className="btn send-button"
+                      value="Send Message"
+                    />
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div className="col-lg-6 col-md-6 d-flex align-items-stretch contact-text">
+              <div
+                data-aos="fade-right "
+                className="text-center info-wrap w-100 pl-2"
+              >
+                <img className="w-75 h-auto mr-8 fluid-img" src={logo} alt="" />{" "}
+                <br /> <br />
+                <h4 className="contact-tag ">
+                  MASTER YOUR MIND WITH <br /> Solution-Focused Hypnotherapy
+                </h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default Contact
+export default Contact;
--- src/MegaServiceMenu.js
+++ src/MegaServiceMenu.js
@@ -1,82 +1,86 @@
-import React,{useState, useEffect} from 'react'
-import './mega.css'
+import React, { useState, useEffect } from "react";
+import "./mega.css";
 import axios from "axios";
 import { api, IMG_BASE_URL } from "./api/api";
-import { Button } from './Button';
+import { Button } from "./Button";
 import "./pricesection.css";
 import "./infosection.css";
+import { BiChevronDown } from "react-icons/bi";
 const MegaServiceMenu = () => {
-    const [program, setAllProgram] = useState([]);
-    const [navbar, setNavbar] = useState(false)
-    useEffect(() => {
-        axios
-          .get(api.allProgramm)
-          .then((res) => {
-    
-            setAllProgram(res.data.data);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      }, []);
+  const [program, setAllProgram] = useState([]);
+  const [navbar, setNavbar] = useState(false);
+  useEffect(() => {
+    axios
+      .get(api.allProgramm)
+      .then((res) => {
+        setAllProgram(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <>
-    <div class="ruby-menu-demo-header">
-     
+      <div class="ruby-menu-demo-header">
         <ul class="ruby-menu">
           <li class="ruby-menu-mega-blog package">
-            <li className="nav-link" href="#">SERVICES🢓
-            
+            <li className="nav-link" href="#">
+              SERVICES
+              <BiChevronDown />
             </li>
-            
-            <div style={{height: "220px", width: "670px", marginLeft:"-200px"  }} class="">
+
+            <div
+              style={{ height: "220px", width: "670px", marginLeft: "-200px" }}
+              class=""
+            >
               <ul class="ruby-menu-mega-blog-nav">
-                
-                     {program.map((val, ind) => {
-                        return (
-                          <>
-                            <li class="ruby-active-menu-item">
-                  <a href="#">{val.title}</a>
-                  <div
-                    class="ruby-grid ruby-grid-lined"
-                    style={{height: "220px", width: "700px"}}
-                  >
-                    <div class="ruby-row">
-                      <div class="ruby-col-4">
-                        <img src={`${IMG_BASE_URL}/services/${val.photo}`} style={{marginBottom: "50px"}}/>
-                      </div>
-                      <div class="ruby-col-4">
-                        <span class="ruby-c-title ruby-margin-10">
-                          <a href={"#"}>{val.title}</a>
-                        </span>
-                        <span class="ruby-c-content">
-                     
-                       {val.details.substring(0, 200)}
-                          <br />
-                          <Button to={`/program/${val.id}`} className="home-button"   style={{marginTop: "10px"}}>
-                            read more
-                          </Button>
-                        </span>
-                      </div>
-                    </div>
-                  </div>
-                  <span class="ruby-dropdown-toggle"></span>
-                </li>
-                            
-                          </>
-                        );
-                      })}
-                
-               
+                {program.map((val, ind) => {
+                  return (
+                    <>
+                      <li class="ruby-active-menu-item">
+                        <a href="#">{val.title}</a>
+                        <div
+                          class="ruby-grid ruby-grid-lined"
+                          style={{ height: "220px", width: "700px" }}
+                        >
+                          <div class="ruby-row">
+                            <div class="ruby-col-4">
+                              <img
+                                src={`${IMG_BASE_URL}/services/${val.photo}`}
+                                style={{ marginBottom: "50px" }}
+                              />
+                            </div>
+                            <div class="ruby-col-4">
+                              <span class="ruby-c-title ruby-margin-10">
+                                <a href={"#"}>{val.title}</a>
+                              </span>
+                              <span class="ruby-c-content">
+                                {val.details.substring(0, 200)}
+                                <br />
+                                <Button
+                                  to={`/program/${val.id}`}
+                                  className="home-button"
+                                  style={{ marginTop: "10px" }}
+                                >
+                                  read more
+                                </Button>
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                        <span class="ruby-dropdown-toggle"></span>
+                      </li>
+                    </>
+                  );
+                })}
               </ul>
             </div>
             <span class="ruby-dropdown-toggle"></span>
           </li>
         </ul>
-      
-    </div>
-  </>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default MegaServiceMenu
+export default MegaServiceMenu;

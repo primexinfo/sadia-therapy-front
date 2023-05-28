716d090c4b408cf062142e89d03ed5d49b3706c0
Mizanur Rahman <mizanurrahman615@gmail.com>
2023-05-28 19:36:32 +0600

    redirect page edit

--- src/CheckoutForm.js
+++ src/CheckoutForm.js
@@ -62,7 +62,7 @@ const CheckoutForm = (amount, remail) => {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "http://localhost:3000/thank-you",
+        return_url: "https://sadiatherapy.com/thank-you",
       },
     });
 

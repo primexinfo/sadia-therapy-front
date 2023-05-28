1c70e8926386da092878ae1f455743d921a89fd5
Mizanur Rahman <mizanurrahman615@gmail.com>
2023-05-28 19:31:12 +0600

    congratulation notifire added.

--- src/thankyou.js
+++ src/thankyou.js
@@ -1,19 +1,24 @@
 import React from "react";
 import image from "../src/image/cong.gif"
+import { useWindowSize } from "react-use";
+import ReactConfetti from "react-confetti";
 
 
 const Thankyou = () => {
     document.title = "Congratulations! Thanks for your payment";
+    const { width, height } = useWindowSize();
     return(
-        <div className="row rml justify-content-center">
+        <div className="row rml justify-content-center" style={{minHeight: "400px"}}>
+            <ReactConfetti width={width} height={height} />
             <div className="col-sm-12 col-md-6">
                 <div className="modal-content anim mb-4">
                     <div className="modal-header p-4">
                         <h3 className="modal-title" id="modalRequestLabel">
                         Thanks for your payment.
                         </h3>
+                        
                     </div>
-                        <img src={image} className="w-100" alt="CongratulationS" />
+                    <img src={image} alt="Congratulations" className="mt-5" />
                 </div>
             </div>
         </div>
